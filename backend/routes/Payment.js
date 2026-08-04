2a4000555b929ed922b7b972cbae98ee3846ea0b
Vier.dev <mnisinghweiry@gmail.com>
2026-08-04 19:53:20 +0700

    หกด

--- backend/routes/Payment.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const bookingController = require('../controllers/booking')
-
-// ใช้งาน
-router.post('/bookings', bookingController.createBooking)
-
-module.exports = router
